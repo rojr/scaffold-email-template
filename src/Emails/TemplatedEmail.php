a64f2a918b2456c123c2f232ee0ca57b1cc56b32
robmadeyou <robmadeyou@gmail.com>
2017-08-09 22:30:26 +0100

    Making it so that there will only ever be one Base Template

---
+++ src/Emails/TemplatedEmail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Rojr\Scaffold\Email\Templates\Emails;
+
+use Rhubarb\Crown\Sendables\Email\TemplateEmail;
+
+class TemplatedEmail extends TemplateEmail
+{
+    protected function getTextTemplateBody()
+    {
+    }
+
+    protected function getHtmlTemplateBody()
+    {
+    }
+
+    protected function getSubjectTemplate()
+    {
+    }
+}
--- src/Models/EmailTemplate.php
+++ src/Models/EmailTemplate.php
@@ -3,9 +3,16 @@
 namespace Rojr\Scaffold\Email\Templates\Models;
 
 use Rhubarb\Crown\String\StringTools;
+use Rhubarb\Leaf\Table\Leaves\Columns\BooleanColumn;
+use Rhubarb\Stem\Exceptions\RecordNotFoundException;
+use Rhubarb\Stem\Filters\AndGroup;
+use Rhubarb\Stem\Filters\Equals;
+use Rhubarb\Stem\Filters\Not;
 use Rhubarb\Stem\Models\Model;
 use Rhubarb\Stem\Schema\Columns\AutoIncrementColumn;
+use Rhubarb\Stem\Schema\Columns\ForeignKeyColumn;
 use Rhubarb\Stem\Schema\Columns\LongStringColumn;
+use Rhubarb\Stem\Schema\Columns\StringColumn;
 use Rhubarb\Stem\Schema\ModelSchema;
 
 class EmailTemplate extends Model
@@ -16,11 +23,34 @@ class EmailTemplate extends Model
 
         $schema->addColumn(
             new AutoIncrementColumn('EmailTemplateID'),
+            new StringColumn('ClassName', 255),
             new StringTools('Subject', 500),
             new StringTools('FromAddress', 100),
-            new LongStringColumn('Content')
+            new LongStringColumn('Content'),
+            new ForeignKeyColumn('ParentEmailTemplate'),
+            new BooleanColumn('IsBase')
         );
 
         return $schema;
+    }
+
+    protected function getConsistencyValidationErrors()
+    {
+        $errors = parent::getConsistencyValidationErrors();
+
+        try {
+            self::find(
+                new AndGroup(
+                    [
+                        new Equals('IsBase', true),
+                        new Not(new Equals($this->getUniqueIdentifierColumnName(), $this->getUniqueIdentifier())),
+                    ]
+                ));
+
+            $errors['IsBase'] = 'Base template already set. There can only be one.';
+        } catch (RecordNotFoundException $ex) {
+        }
+
+        return $errors;
     }
 }
